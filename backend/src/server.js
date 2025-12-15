75ef1b0f89137a5f70d925e3e8480f1ca967b39e
Suhail KC <suhailkc3@gmail.com>
2025-12-15 12:57:46 +0530

    added FORNTEND_URL to env file

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-  origin: 'http://localhost:5173', // frontend URL
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }))
 app.use(cookieParser())
